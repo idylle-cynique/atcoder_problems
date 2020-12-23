a0e0f0b31e13ee6aca30fbab7154204f50f06067
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2020-12-24 00:01:01 +0900

    new answer programs

---
+++ ABC-Like Contest/M-Solutions2020-A.py
@@ -0,0 +1,11 @@
+# M-Solutions プロコンオープン2020 - A
+score = [1800,1600,1400,1200,1000, 800, 600, 400]
+ranks = [   1,   2,   3,   4,   5,   6,   7,   8]
+
+x = int(input())
+i = 0
+
+while(x < score[i]):
+      i += 1
+
+print(ranks[i])
---
+++ AtCoder Begginers Contest/ABC174-A.py
@@ -0,0 +1,2 @@
+# ABC174 - A
+print("Yes") if int(input()) >= 30 else print("No")
