96f42da8e893076e1303b227a23806fcba20a31c
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2020-12-26 20:28:57 +0900

    a problem to try again
    解説AC

--- AtCoder Begginers Contest/ABC136-C.py
+++ AtCoder Begginers Contest/ABC136-C.py
@@ -1,4 +1,5 @@
 # ABC136 - C
+
 # 0~iまでの最大値 max_h を記録しておき、max_h > h[i] のとき "No" とすればどうか
 # 結果的に上手く実装できず解説AC
 
@@ -17,5 +18,4 @@ for i in range(1,n):
             print("No")
             exit()
 
-#print(max_h)
 print("Yes")
