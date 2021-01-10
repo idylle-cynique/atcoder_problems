21356a516992dd365d3132b57891bbc368df6e8f
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2021-01-11 00:45:10 +0900

    Update ABC079-A.py

--- AtCoder Beginners Contest/ABC079-A.py
+++ AtCoder Beginners Contest/ABC079-A.py
@@ -1,9 +1,9 @@
-    threes = ["111","222","333","444","555","666","777","888","999","000"]
-    n = input()
+threes = ["111","222","333","444","555","666","777","888","999","000"]
+n = input()
      
-    for i in threes:
-          if i in n:
-                print("Yes")
-                exit()
+for i in threes:
+      if i in n:
+            print("Yes")
+            exit()
      
-    print("No")
+print("No")
