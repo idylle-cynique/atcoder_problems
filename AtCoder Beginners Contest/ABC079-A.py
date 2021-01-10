b213da837e9edb34a04c8bef57962d0a2254254f
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2021-01-11 00:02:11 +0900

    Create ABC079-A.py

---
+++ AtCoder Beginners Contest/ABC079-A.py
@@ -0,0 +1,9 @@
+    threes = ["111","222","333","444","555","666","777","888","999","000"]
+    n = input()
+     
+    for i in threes:
+          if i in n:
+                print("Yes")
+                exit()
+     
+    print("No")
