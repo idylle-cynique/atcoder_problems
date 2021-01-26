a6c7b4c86551f4d2dd53c9a0c477993a6488dc54
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2021-01-26 23:21:53 +0900

    Create ABC122-C.py

---
+++ AtCoder Beginners Contest/ABC122-C.py
@@ -0,0 +1,19 @@
+n,q = [int(x) for x in input().split()]
+s = input()
+accm = [0]
+cnt = 0
+
+for i in range(1,n):
+      if s[i-1:i+1] == "AC":
+            cnt += 1
+            accm.append(cnt)
+      else:
+            accm.append(cnt)
+
+#print(len(accm),accm); print(len(s)   ,(s))
+
+for i in range(q):
+      l,r = [int(x) for x in input().split()]
+      l,r = l-1,r-1
+      #print(s[l:r]);
+      print(accm[r]-accm[l])
