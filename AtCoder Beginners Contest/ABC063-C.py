c7eef0cefa416da1df7e68bf7f3483f3ef489b78
anecdoche <65955152+idylle-cynique@users.noreply.github.com>
2021-02-07 21:01:52 +0900

    Update ABC063-C.py
    コメントを追加

--- AtCoder Beginners Contest/ABC063-C.py
+++ AtCoder Beginners Contest/ABC063-C.py
@@ -8,24 +8,25 @@ total = 0
 
 for i in range(n):
       temp = int(input())
-      total += temp
+      total += temp     # 満点時の得点数を調べておく
       
-      if temp%10 == 0:
+      if temp%10 == 0:  # 10を約数に持つ得点の問題を格納
             tens.append(temp)
-      else:
+      else:             # 10を約数に持たない得点の問題を格納
             nottens.append(temp)
 
 nottens = sorted(nottens)
+                        # 扱いやすいようにソートしておく
 #print("tens   :",tens); print("nottens:",nottens)
-if total%10 != 0:
+if total%10 != 0:       # 満点が10の倍数でないならそのまま満点を出力
       print(total)
       exit()
       
-if len(nottens) == 0:
+if len(nottens) == 0:   # 10を約数に持たない得点の問題が存在しないときは、どうやっても0点にしかならない
       print(0)
       exit()
 
-for i in nottens:
+for i in nottens:       # 小さいものから順に差し引いて、10の倍数でなくなった時点で出力
       total -= i
       if total%10 != 0:
             print(total)
