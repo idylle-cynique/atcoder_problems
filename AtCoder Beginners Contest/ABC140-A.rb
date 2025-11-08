b199be9fa21985de6c250c756335ee0fc8064926
anecdotes <maltese_harvest@aol.com>
2025-11-09 01:30:52 +0900

    add: ABC140

---
+++ AtCoder Beginners Contest/ABC140-A.rb
@@ -0,0 +1,12 @@
+# ABC 140 - A
+# https://atcoder.jp/contests/abc140/tasks/abc140_a
+
+def calc(n)
+  n ** 3
+end
+
+N = gets.to_i
+
+answer = calc(N)
+
+p answer
--- AtCoder Beginners Contest/ABC140-C.py
+++ AtCoder Beginners Contest/ABC140-C.py
@@ -1,4 +1,5 @@
 # ABC140 - C
+# https://atcoder.jp/contests/abc140/tasks/abc140_c
 
 # 解となる数列を得るには、B_i >= max(A_i,A_i+1)と逆のことをすればよい
 # 数列Bを反転させて、A[0] = B[0]としたあと、min(B[i],B[i+1])を順番に数列Aに格納していく
@@ -8,23 +9,21 @@ b = [int(x) for x in input().split()]
 b = b[::-1]
 a = [b[0]]
 total = b[0]
-#print(b)
 
 if N-1 == 1 or N-1 == 2: # コーナーケース(1)
     total += b[-1]
     print(total)
     exit()
-    
+
 if N-1 == 2: # コーナーケース(2)
     total += (min(b)+b[-1])
     print(total)
     exit()
-    
+
 for i in range(N-1-1):
-    #print(b[i],b[i+1]); print(a)
     a.append(min(b[i],b[i+1]))
     total += min(b[i],b[i+1])
 
 total += b[-1]
 
-print(total)#; print(a) 
+print(total)
